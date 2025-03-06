563817509f6b5493194d405c9cbd7cf5804d0011
kd <yadavkalidas4@gmail.com>
2025-03-06 14:51:16 +0530

    added p tag

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      <p>hello world </p>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
